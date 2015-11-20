b4c7f416ff2ba73bd0d3ee25c0563a2c3e8053c9
Remco Tolsma <info@remcotolsma.nl>
2015-11-20 12:23:30 +0100

    Added custom config factory.

---
+++ src/ConfigTest.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Title: ING - iDEAL Advanced - v3 config test
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2015
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
+class Pronamic_WP_Pay_Gateways_ING_IDealAdvancedV3_ConfigTest extends Pronamic_WP_Pay_Gateways_ING_IDealAdvancedV3_Config {
+	public function get_payment_server_url() {
+		return 'https://idealtest.secure-ing.com/ideal/iDeal';
+	}
+}
